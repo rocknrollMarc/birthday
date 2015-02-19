c0aca38aee81d0873f5ff27688a600577ac88ddb
guillaume jacquart <guillaume@dev.euphor.fr>
2015-02-19 18:18:26 +0100

    Add misc and login.

--- www/js/controllers/welcome.js
+++ www/js/controllers/welcome.js
@@ -1,4 +1,4 @@
-dashboard.controller('welcome', function ($scope, $rootScope, $firebase, $ionicPopup, $ionicSlideBoxDelegate) {
+dashboard.controller('welcome', function ($scope, $rootScope, $location, $firebase, $ionicPopup, $ionicSlideBoxDelegate) {
 	var ref = new Firebase("https://maxou.firebaseio.com/notifications");	
 	var sync = $firebase(ref);
 	sync.$push({ message: "Maxou a ouvert l'appli !", date: new Date().getTime() });
@@ -11,22 +11,24 @@ dashboard.controller('welcome', function ($scope, $rootScope, $firebase, $ionicP
 	}
 	
 	$scope.checkWise = function(test){
-		if(test == 'Oui'){			
+		$scope.wiseImgShow = true;
+		if(test == 'Oui'){	
+			$scope.wiseImg = 'img/coquinou.jpg';
 			var alertPopup = $ionicPopup.alert({
 				title: 'Coquinou!',
 				template: 'Voyons, ne me mens pas, je te connais petit troufion.'
 			});
 			alertPopup.then(function(res) {
-				$scope.wiseValid = true;
 			});
 		}		
 		else if(test == 'Non'){			
+			$scope.wiseImg = 'img/fesses.jpg';
 			var alertPopup = $ionicPopup.alert({
 				title: 'Coquinou!',
 				template: 'Ton honnêteté saura être récompensée.'
 			});
 			alertPopup.then(function(res) {
-				console.log('Thank you for not eating my delicious ice cream cone');
+				$scope.wiseValid = true;
 			});
 		}
 	};
@@ -44,4 +46,26 @@ dashboard.controller('welcome', function ($scope, $rootScope, $firebase, $ionicP
 		$scope.showNext = !(index == $ionicSlideBoxDelegate.slidesCount() - 1);
 		sync.$push({ message: "Maxou est sur le slideshow " + $ionicSlideBoxDelegate.currentIndex() + " !", date: new Date().getTime() });
 	}
+	
+	var ref = new Firebase("https://maxou.firebaseio.com");
+	
+	$scope.login = function(){
+		
+		var test = ref.getAuth();
+		if(test && test.facebook.email == 'guillaume.jacquart@live.fr'){
+			$location.path('app/home');
+			return;
+		}
+		
+		ref.authWithOAuthPopup("facebook", function(error, authData) {
+		  if (error) {
+			console.log("Login Failed!", error);
+		  } else {
+			$location.path('app/home');
+			return;
+		  }
+		}, {
+		  scope: "email"
+		});
+	};
 });
